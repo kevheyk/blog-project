2ad735ae32ef873ef50a32642ca083f3bfd9c572
Kevin He <kevheyk@gmail.com>
2020-05-10 19:54:12 -0600

    Put up static templates

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,25 +7,44 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+// Set Environment
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+// GET Home Route
+app.get("/", function(req, res){
+  res.render("home",{
+    homeStartingContent: homeStartingContent
+  });
+  
+});
+
+// GET About Route
+app.get("/about", function(req, res){
+  res.render("about",{
+    aboutContent: aboutContent
+  });
+  
+});
+
+// GET Contact Route
+app.get("/contact", function(req, res){
+  res.render("contact",{
+    contactContent: contactContent
+  });
+  
+});
+
+
+
+
+
+
+
+
