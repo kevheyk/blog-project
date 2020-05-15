f88662865e809b2980161766ba8b728999db6919
Kevin He <kevheyk@gmail.com>
2020-05-14 21:38:24 -0600

    Add and read posts to database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,17 +19,51 @@ app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
+//Connect to DB
+
+mongoose.connect("mongodb://localhost:27017/blogsDB",{
+   useNewUrlParser: true,
+   useUnifiedTopology: true,}, (err)=>{
+  if (err) {
+    console.log("Error connecting DB: "+err);
+  } else{
+    console.log("DB Connected.")
+  }
+})
+
+//Creat collections
+
+const blogSchema = new mongoose.Schema({
+  postTitle:{
+    type:String,
+    required:true
+  },
+  postBody:{
+    type: String,
+    required :true
+  }
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+
 // Global Var
 
 const posts = [];
 console.log(posts);
 // GET Home Route
 app.get("/", function(req, res){
-  res.render("home",{
-    homeStartingContent: homeStartingContent,
-    posts: posts
-  });
 
+  
+    Blog.find({},(err, docs)=>{
+      if (err){
+        console.log("Error Finding: " + err)
+      } else {
+        res.render("home",{
+          homeStartingContent: homeStartingContent,
+          posts: docs
+        });
+      }
+    })
 });
 
 
@@ -50,40 +85,71 @@ app.get("/contact", function(req, res){
 
 // GET Compose Route
 app.get("/compose", function(req, res){
-  res.render("compose",{
-    contactContent: contactContent
-  });
+  res.render("compose")
   
 });
 
 app.post("/compose", function(req, res){
   
-  const post = {
+  const newPost = new Blog({
     postTitle: req.body.postTitle,
     postBody: req.body.postBody
-  };
-  posts.push(post)
-  res.redirect("/");
-
-
+  });
+  // posts.push(post)
+  newPost.save({validateBeforeSave:true}, (err)=>{
+    if (err){
+      console.log("Erro saving new post: "+ err)
+    } else{
+      Blog.find({}, (err,docs)=>{
+        if(err){
+          console.log(err);
+        } else {
+          console.log(docs);
+          res.redirect("/")
+        }
+      });
+      
+    }
+  })
 });
 
 
 // GET PostID
 app.get("/posts/:postID", function(req, res){
   console.log(req.params.postID);
-  if(posts.filter(post => _.lowerCase(post.postTitle) === _.lowerCase(req.params.postID)).length>0){
-    console.log("Match Found")
-    res.render("post",{
-      post: posts.filter(post => _.lowerCase(post.postTitle) === _.lowerCase(req.params.postID))[0]
-    });
-  } else{
-    console.log("Post Not Found")
-    res.redirect("/");
-  }
+  // if(posts.filter(post => _.lowerCase(post.postTitle) === _.lowerCase(req.params.postID)).length>0){
+  //   console.log("Match Found")
+  //   res.render("post",{
+  //     post: posts.filter(post => _.lowerCase(post.postTitle) === _.lowerCase(req.params.postID))[0]
+  //   });
+  // } else{
+  //   console.log("Post Not Found")
+  //   res.redirect("/");
+  // }
   
+  // Find post object from DB using post title
+  const postTitle = req.params.postID;
+  Blog.findOne({postTitle:postTitle},(err,doc)=>{
+    if(err){
+      console.log("Error directing to post: " + err)
+    } else{
+      res.render("post", {post:doc})
+    }
+  })
+
 })
 
+// Delete All
+app.get("/delete-all",(req, res)=>{
+  Blog.deleteMany({},(err)=>{
+    if (err){
+      console.log("Error Deleting: " +err)
+    } else {
+      res.redirect("/")
+    }
+  })
+
+})
 
 
 
